d99d8c16caa2b012a9d6ccb55e5a082def30f12e
Diana Viglucci <dvigl86@gmail.com>
2022-02-22 11:06:06 -0500

    worked on css

--- app/main.js
+++ app/main.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './store.js';
 import { Provider } from 'react-redux';
+// import '../public/index.css';
 
 ReactDOM.render(
   <Provider store={store}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,29 @@
+const webpack = require('webpack'); 
+
 module.exports = {
-    mode: 'development',
-    entry: [
-      './app/main.js'
+  mode: "development",
+  entry: "./app/main.js",
+  output: {
+    path: __dirname,
+    filename: "./public/bundle.js",
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+      // use the style-loader/css-loader combos for anything matching the .css extension
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-    output: {
-      path: __dirname,
-      filename: './public/bundle.js'
-    },
-    devtool: 'source-map',
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-react'
-            ]
-          }
-        }
-      ]
-    }
-  }
+  },
+};
