c9cfd712bb9a4af76ee65ef722b2edc826f3c86b
Diana Viglucci <dvigl86@gmail.com>
2022-02-22 10:12:08 -0500

    completed redux section

--- app/main.js
+++ app/main.js
@@ -1,8 +1,12 @@
 // ultimately, you will also import and place Provider and Routes in this file
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './store.js';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <div>Hello, world!</div>,
+  <Provider store={store}>
+    <div>Hello, world!</div>
+  </Provider>,
   document.getElementById('app') // make sure this is the same as the id of the div in your index.html
 );
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // created a separate file to start the server and listen for requests
-const app = require('./server');
+const app = require('./server/index.js');
 const port = process.env.PORT || 3000; // this can be very useful if you deploy to Heroku!
 
 app.listen(port, function () {
